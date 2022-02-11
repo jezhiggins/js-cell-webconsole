f073c366335ae5d6a57355b2fbe131285d6648e0
Jez Higgins <jez@jezuk.co.uk>
2022-02-11 22:53:26 +0000

    redirect print function

--- src/components/eval-viewer/index.js
+++ src/components/eval-viewer/index.js
@@ -2,6 +2,7 @@ import {lex} from "cell-lang/lib/lexer";
 import Viewer from "../viewer";
 import {parse} from "cell-lang/lib/parser";
 import {evaluate} from "cell-lang/lib/evaluator";
+import {topLevelEnvironment} from "cell-lang/lib/environment";
 
 const allParse = code => {
   try {
@@ -11,12 +12,27 @@ const allParse = code => {
   }
 }
 
-const evaluator = code => {
+let outputBuffer = "";
+function webPrintFn(env, value) {
+  outputBuffer += `${value}\n`;
+  return ['none', null]
+}
+
+const env = topLevelEnvironment().newScope();
+env.set("print", ['native', webPrintFn]);
+
+const evalCell = code => {
   try {
-    return evaluate(allParse(code));
+    return evaluate(allParse(code), env);
   } catch(e) {
     return e.message
   }
+}
+
+const evaluator = code => {
+  outputBuffer = "";
+  const result = evalCell(code);
+  return outputBuffer ? `${outputBuffer}\n${result}` : result;
 }
 
 const EvalViewer = ({ code }) => {
